47783c1605c5a63b419a843ab52ee02843887527
RiadHossain43 <riadhossain7464@gmail.com>
2022-12-16 04:44:30 +0600

    data validation errors api response handled properly

--- src/common/middleware/validator.js
+++ src/common/middleware/validator.js
@@ -1,12 +1,15 @@
 const { Validation } = require("../helper/validation");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const validate =
   (validationObjectName) => (schema) => async (req, res, next) => {
     const validation = new Validation(req.tenant);
     const errors = validation.validate(schema, req[validationObjectName]);
     if (errors)
-      return res.status(400).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
         message: `${validationObjectName} validation error.`,
-        errors,
+        details: {
+          errors,
+        },
       });
     next();
   };
